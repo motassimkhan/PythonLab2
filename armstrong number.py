9eb02413471536b5a659a2b9abbf92f14fe06a7d
unknown <hbeww@sfw.com>
2023-10-12 15:32:26 +0530

    armstrong

--- armstrong number.py
+++ armstrong number.py
@@ -1,8 +1,12 @@
 n = int(input("Enter number: "))
+a=n
 arm = 0
 while n>0:
     rem = n%10
     arm = arm+ (rem*rem*rem)
     n//=10
-print(arm)
     
+if arm == a :
+    print(a,' is armstrong')
+else:
+    print("not armstrong",arm)
