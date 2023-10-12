81a44b8af9fbdfe1f0dfea4e4ffb7d43a15b4359
unknown <hbeww@sfw.com>
2023-10-12 14:19:45 +0530

    factorial

---
+++ factorial.py
@@ -0,0 +1,8 @@
+n = int(input("Enter the number: "))
+f = 1
+if n == 1 or n == 0:
+    print(f'Factorial of {n} is 1')
+else:
+    for i in range(2,n+1):
+        f = f*i
+    print(f'Factorial of {n} is {f}')
